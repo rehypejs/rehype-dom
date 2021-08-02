74aba95c1ca54084bed019b7c77d1f3de9165eec
Titus Wormer <tituswormer@gmail.com>
2021-08-02 23:36:59 +0200

    Update `xo`

--- packages/rehype-dom-parse/index.d.ts
+++ packages/rehype-dom-parse/index.d.ts
@@ -1,7 +1,8 @@
 // This wrapper exists because JS in TS can’t export a `@type` of a function.
-import type {Options} from './lib/index.js'
 import type {Root} from 'hast'
 import type {Plugin} from 'unified'
+import type {Options} from './lib/index.js'
+
 declare const rehypeDomParse: Plugin<[Options?] | void[], string, Root>
 export default rehypeDomParse
 export type {Options}
--- packages/rehype-dom-stringify/index.d.ts
+++ packages/rehype-dom-stringify/index.d.ts
@@ -1,7 +1,8 @@
 // This wrapper exists because JS in TS can’t export a `@type` of a function.
-import type {Options} from './lib/index.js'
 import type {Root} from 'hast'
 import type {Plugin} from 'unified'
+import type {Options} from './lib/index.js'
+
 declare const rehypeDomStrigify: Plugin<[Options?] | void[], Root, string>
 export default rehypeDomStrigify
 export type {Options}
